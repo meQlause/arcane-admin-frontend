a0de744fc5f4b5ee4c3d1d7961c9297f511b1684
degiam <degiam@ymail.com>
2024-01-26 23:43:29 +0700

    reinit params

--- app/proposal/member.tsx
+++ app/proposal/member.tsx
@@ -3,6 +3,11 @@ import { MainTitle } from "@/app/components/main";
 
 export default function ProposalMember({ rdt }: any) {
   const { account } = useAccount({ rdt })
+  const profile: any = {
+    username: account?.address,
+    avatar: '/user/user-1.png',
+    role: 'Admin'
+  }
 
   return (
     <>
@@ -10,9 +15,9 @@ export default function ProposalMember({ rdt }: any) {
         <>
           <MainTitle
             title={`Proposal`}
-            userName={account.address}
-            userImage={`/user/user-1.png`}
-            userStatus={`Core`}
+            userName={profile.username}
+            userImage={profile.avatar}
+            userRole={profile.role}
           />
     
           <p>Member page</p>

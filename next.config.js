f5f38e1524e7f4e7df172ef0d09f6ccefe392cde
Qlause22 <qlausebrian@gmail.com>
2024-03-02 19:29:58 +0900

    configure the ssl cert

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+import fs from 'fs';
+import https from 'https';
+
+// Load the certificate
+const cert = fs.readFileSync('./app/cert/cert.crt');
+
+// Add the certificate to the global agent
+https.globalAgent.options.ca = cert;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,4 +21,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
